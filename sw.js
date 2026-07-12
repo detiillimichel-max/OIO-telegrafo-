d47e032731aff9de27335768b60971871df12c7d
Michel Detiilli <detiillimichel@gmail.com>
2026-07-12 18:48:10 -0300

    Update cache version to 'oio-telegrafo-v9'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='oio-telegrafo-v3';
+const CACHE='oio-telegrafo-v9';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./icon-192.png','./icon-512.png'])).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.url.includes('supabase'))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
